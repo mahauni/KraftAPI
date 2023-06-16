f2ad91b2e87421c40fd3e6dfa9ddb34342eb7603
Lucas Raoni <lucasraoniha@gmail.com>
2023-06-16 19:01:09 -0300

    Initial commit

---
+++ src/main/java/com/br/ese/KraftAPI/KraftApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.ese.KraftAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KraftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KraftApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/ese/KraftAPI/KraftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ese.KraftAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KraftApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
